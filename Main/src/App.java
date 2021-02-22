8e51d204bef630ff981d857527ab96bb4a228ac0
Arpita-22 <duttaarpita22@gmail.com>
2021-02-22 14:58:54 -0800

    Static class attribute

--- Main/src/App.java
+++ Main/src/App.java
@@ -2,30 +2,11 @@
 import java.util.Scanner;
 
 public class App {
-	public static void main(String[]args) {
-		String q1 = "color of sky?\n"+ "(a) red\n (b) blue\n";
-		String q2 = "color of wood?\n" + "(a) brown\n (b)green\n" ;
-		
-		Question [] questions = {
-				new Question(q1, "b"),
-				new Question(q2, "a")
-		};
-		
-		taketest(questions);
-		
-	}
 	
-	public static void taketest(Question []questions) {
-		int score = 0;
-		Scanner input = new Scanner (System.in);
-		for(int i = 0; i < questions.length; i++) {
-			System.out.println(questions[i].prompt);
-			String answer = input.nextLine();
-			if(answer.equals(questions[i].answer)) {
-				score++;
-			}
-		}
-		System.out.println(score);
+	public static void main(String[]args) {	
+		Student student1 = new Student("Karl", 3.7);
+		Student student2 = new Student("Jim", 3.0);
+		System.out.println(student1.getStudentCount());
 	}
 
 }
--- Main/src/Student.java
+++ Main/src/Student.java
@@ -1,16 +1,30 @@
 
 public class Student {
-	String name;
+	private String name;
 	double gpa;
+	private static int studentCount = 0;
 	
 	public Student(String name, double gpa) {
-		this.name = name;
+		this.setName(name);
 		this.gpa = gpa;
+		studentCount++;
+		System.out.println(studentCount);
+	}
+	
+	public int getStudentCount() {
+		return studentCount;
 	}
 	
 	public boolean IsOnHonor() {
 		if(gpa >= 3.5) return true;
 		return false;
 	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
