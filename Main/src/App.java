1629203e51052098d478134a2d691aa9099d59e1
Arpita-22 <duttaarpita22@gmail.com>
2021-02-22 17:59:51 -0800

    Interface inheritance

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,12 +1,12 @@
 
-import java.util.Scanner;
 
 public class App {
 	
 	public static void main(String[]args) {	
-		Student student1 = new Student("Karl", 3.7);
-		Student student2 = new Student("Jim", 3.0);
-		System.out.println(student1.getStudentCount());
+		
+		Animal dog1 = new Dog ();
+		dog1.eat();
+		
 	}
 
 }
---
+++ Main/src/Dog.java
@@ -0,0 +1,16 @@
+
+public class Dog implements Animal{
+
+	@Override
+	public void speak() {
+		System.out.println("woof");
+		
+	}
+
+	@Override
+	public void eat() {
+		System.out.println(" Eats biscuit");
+		
+	}
+
+}
