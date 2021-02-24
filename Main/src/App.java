c5178f60513b501e2e7f236eabea7e0546b02175
Arpita-22 <duttaarpita22@gmail.com>
2021-02-23 16:21:39 -0800

    TThread and Runnable

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,21 +1,114 @@
-import java.util.ArrayList;
+//class Task{
+//	void executeTask() {
+//		System.out.println("execute task");
+//	}
 
-public class App {
+
+class Task extends Thread{
+	@Override
+	public void run() {
+		for(int i = 0; i <= 10; i++) {
+			System.out.println("@@ run thread1" + " " + i);
+		}
+	}	
+}
+
+//Using Thread class
+
+//public class App extends Thread {
+ public class App{
 	
+//	public void run() {
+//		for(int i = 0; i <= 10; i++) {
+//			System.out.println("run thread1" +  i);
+//		}
+
+
+	//main method represents main thread
 	public static void main(String[]args) {	
-		ArrayList<Integer> num = new ArrayList<Integer>();
-		num.add(5);
-		num.add(10);
-		Double mydouble = 3.5;
-		System.out.println(mydouble);
-		System.out.println(mydouble.doubleValue());
-		for(int i: num) {
-			System.out.println(i);
+
+		//everything here would be executed by main thread
+		//Threads always execute code in a sequence
+		
+		System.out.println("---App Started---");
+		
+		//main and my task are executing parallely and separately
+		
+		Task mytask = new Task();
+//		mytask.executeTask();
+		 mytask.start();
+		
+		for(int i = 0; i <= 10; i++) {
+			System.out.println("$$ run thread2" + " " + i);
 		}
 		
+//		App obj = new App();
+		//start internally executes run method
+//		obj.start();
+		
+		System.out.println("---App Finished---");
 	}
-
+	
 }
+ 
+		
+
+//Using runnable interface because multiple inheritance not supported in java
+
+//public class App implements Runnable {
+//	
+//	public void run() {
+//		System.out.println("run thread");
+//	}
+//
+//	public static void main(String[]args) {	
+//		Runnable r = new App();
+//        Thread obj = new Thread(r);
+//        obj.start();
+//		
+//		Thread obj = new Thread(new App());
+//		obj.start();
+ 
+//     	Thread obj2 = new Thread(new Task2());
+//		obj2.start();
+//		
+//		
+//		
+//	}
+//	
+//}
+ 
+// class Task2 implements Runnable{
+//		@Override
+//		public void run() {
+//			for(int i = 0; i <= 10; i++) {
+//				System.out.println("@@ run thread1" + " " + i);
+//			}
+//		}	
+//	}
+ 
+//public class App{
+//	
+//	public void run() {
+//		System.out.println("run thread");
+//	}
+//
+//	public static void main(String[]args) {	
+//		Runnable r = new App();
+//     Thread obj = new Thread(r);
+//     obj.start();
+//		
+//		Thread obj = new Thread(new App());
+//		obj.start();
+
+//  	Thread obj2 = new Thread(new Task2());
+//		obj2.start();
+//		
+//		
+//		
+//	}
+//	
+//}
 
 
 
